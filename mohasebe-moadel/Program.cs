dc6260d90c6ca3d457dedb89988e0988b6ce6da5
Mohamad alavi <Mohamad alavi@DESKTOP-9QCQADM>
2023-06-24 16:54:49 -0700

    Add project files.

---
+++ mohasebe-moadel/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace mohasebe_moadel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("nomre mian term ra vared konid");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("nomre payan term ra vared konid");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+
+            static Double miangin(double a, double b)
+            {
+                double mianterm = (a * 100) / 35;
+                double payanterm = (b * 100) / 65;
+                return mianterm + payanterm;
+            }
+            Console.ReadKey();
+        }
+    }
+}
